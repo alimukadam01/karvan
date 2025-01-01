f25859fb442be7448d2e567c5493888d9b75635a
Ali Murtaza Mukadam <amurtaza907@gmail.com>
2025-01-02 01:53:16 +0500

    finalized cart page design

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,35 +1,65 @@
 import React from 'react'
 import './Cart.css'
+import productImage from '../assets/batch-001-01.png'
 
 function Cart() {
   return (
     <div className='cart-and-summary-container'>
         <div className="cart-container">
             <h1>Items in your bag</h1>
-            <div className="cart">
-                <div className="table table-customized">
-                    <thead className='table-row'>
-                        <tr className='table-row'>
-                            <th scope="col"></th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Size</th>
-                            <th scope="col">Price</th>
-                            <th scope="col">Quantity</th>
-                            <th scope="col">Total</th>
-                        </tr>
-                    </thead>
-
-                    <tr className='table-row'>
-                        <td></td>
-                        <td>Mark</td>
-                        <td>Medium</td>
-                        <td>3000</td>
-                        <td>2</td>
-                        <td>6000</td>
+            <table className="table all-table">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Product</th>
+                        <th>Size</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
+                        <th>Total</th>
+                        <th></th>
                     </tr>
-                </div>
-            </div>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>
+                            <div className="cart-product-image">
+                                <img src={productImage}/>
+                            </div>
+                        </td>
+                        <td>IOYM Hoodie 01</td>
+                        <td>L</td>
+                        <td>2</td>
+                        <td>3000</td>
+                        <td>6000</td>
+                        <td>
+                            <button type="button" className="tblBtn">
+                                <i className="fa fa-trash" aria-hidden="true"></i>
+                            </button>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <div className="cart-product-image">
+                                <img src={productImage}/>
+                            </div>
+                        </td>
+                        <td>IOYM Hoodie 01</td>
+                        <td>M</td>
+                        <td>2</td>
+                        <td>3000</td>
+                        <td>6000</td>
+                        <td>
+                            <button type="button" className="tblBtn">
+                                <i className="fa fa-trash" aria-hidden="true"></i>
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
+
         <div className="order-summary">
             <h4>Order Summary</h4>
             <div className="summary-items">
